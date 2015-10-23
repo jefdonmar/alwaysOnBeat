6efcfa59b23128f6c7f10077bb60ea80cdb521f8
Jeffrey Martín <jefdonmar@gmail.com>
2015-10-23 14:27:47 -0400

    added a few sound URLs for player victories, intro music, collide noises.. etc. to main.js

--- js/main.js
+++ js/main.js
@@ -7,11 +7,22 @@ console.log('Hello, World');
 //Sounds 
   //Referencing URL with JS to play sound --> http://stackoverflow.com/questions/10142867/reference-url-with-javascript-to-play-sound
 
+    //Intro Music "Girls Love Beyonce Remix" : https://www.youtube.com/watch?v=x1D1I2PTjpQ
+
+    //Choose your Dancer "Mortal Combat Music" : https://www.youtube.com/watch?v=54degO-D4qw
+    //Comp Collision and Player mistake sound " Whoop sees" : https://www.youtube.com/watch?v=XRAkFTjk2qM
+
+    //Sound Effect before each level and player shift " 3..2..1..go Super Smash": https://www.youtube.com/watch?v=jcZntX0AhAU
+    // You Loose sound "sad trombone" : https://www.youtube.com/watch?v=iMpXAknykeg
+
     //Level 1 "Super Mario Bros. Music- Ground Theme" : https://www.youtube.com/watch?v=wGX4obVl64wm
     //Level 2 "Pokemone Blue/Red- Wild Pokemon Battle" : https://www.youtube.com/watch?v=NrS523dOHU4
     //Level 3 "Megaman V- Boss Battle" : https://www.youtube.com/watch?v=vTAkXATZINo
     //Level 4 "Kirby's Dreamland 2 OST- Its Boss Time" : https://www.youtube.com/watch?v=PkAxvvqKrGU
     //Level 5 "Original Tetris Theme" : https://www.youtube.com/watch?v=NmCCQxVBfyM
+
+    // Beyonce Wins "7/11" : https://www.youtube.com/watch?v=OSAOsm1u-OE
+    // Drake Wins " Hotline Bling" : https://www.youtube.com/watch?v=IwH4E3qcjAU
 
 //GIFs
 
