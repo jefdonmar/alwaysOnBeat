660c1428e4641951320dfb6495ec4171b8cab139
Jeffrey Martín <jefdonmar@gmail.com>
2015-10-23 13:09:29 -0400

    Added the static and collide GIF URLs for all game Levels on main.js

--- js/main.js
+++ js/main.js
@@ -4,5 +4,36 @@ import moment from 'moment';
 
 console.log('Hello, World');
 
+//Sounds 
+  //Referencing URL with JS to play sound --> http://stackoverflow.com/questions/10142867/reference-url-with-javascript-to-play-sound
 
-//Referencing URL with JS to play sound --> 
+    //Level 1 "Super Mario Bros. Music- Ground Theme" : https://www.youtube.com/watch?v=wGX4obVl64wm
+    //Level 2 "Pokemone Blue/Red- Wild Pokemon Battle" : https://www.youtube.com/watch?v=NrS523dOHU4
+    //Level 3 "Megaman V- Boss Battle" : https://www.youtube.com/watch?v=vTAkXATZINo
+    //Level 4 "Kirby's Dreamland 2 OST- Its Boss Time" : https://www.youtube.com/watch?v=PkAxvvqKrGU
+    //Level 5 "Original Tetris Theme" : https://www.youtube.com/watch?v=NmCCQxVBfyM
+
+//GIFs
+
+  // Level 1
+    //Drake : https://media3.giphy.com/media/unRa3ffljnuec/200.gif
+    //Beyonce: https://media.giphy.com/media/lhpq7TymdPWeY/giphy.gif
+
+  //Level 2 
+    // Drake: https://media.giphy.com/media/AyKxojJczZ1Li/giphy.gif
+    // Beyonce: https://media.giphy.com/media/pVyytzhmbTkrK/giphy.gif
+
+  //Level 3
+    // Drake: https://media0.giphy.com/media/iuuwqzPEDAZqM/200.gif
+    // Beyonce: https://media0.giphy.com/media/NnFncYOehyCje/200.gif
+
+  //Level 4
+    //Drake and Meek Mills Collide: https://media3.giphy.com/media/ecyGHoXP8Un28/200.gif
+    //Drake: https://media4.giphy.com/media/vsyKKf1t22nmw/200.gif
+
+    //Beyonce and Michelle Collide: https://media4.giphy.com/media/t6nb9PyESsdCo/200.gif
+    //Beyonce: https://media4.giphy.com/media/5rUG5e98jvlhm/200.gif
+
+  //Level 5
+    //Drake: https://media3.giphy.com/media/3o85xJohCZUc524lSU/200.gif
+    //Beyonce: https://media0.giphy.com/media/RX7N03MEUafW8/200.gif 
